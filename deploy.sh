c24c740f91402f95ef49972cff69cc61c65925b2
Mohamed Sallam <sallamx99@gmail.com>
2019-05-16 22:31:35 +0200

    deploy to sallam.dev

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 
-BUCKET_NAME=ci-cd.ga
+BUCKET_NAME=egyptjs.sallam.dev
 
 gsutil -m rsync -rdJ dist gs://$BUCKET_NAME
 
-gsutil -m rsync -rdJ dist gs://www.$BUCKET_NAME
 
 gsutil iam ch allUsers:objectViewer gs://$BUCKET_NAME
 
-gsutil iam ch allUsers:objectViewer gs://www.$BUCKET_NAME
 
 gsutil web set -m index.html -e index.html gs://$BUCKET_NAME
 
-gsutil web set -m index.html -e index.html gs://www.$BUCKET_NAME
 
 gsutil setmeta -h "Content-Type:text/html" \
   -h "Cache-Control:private, max-age=0, no-transform" \
   -h "Content-Disposition: inline" gs://$BUCKET_NAME/**/*.html
-
-gsutil setmeta -h "Content-Type:text/html" \
-  -h "Cache-Control:private, max-age=0, no-transform" \
-  -h "Content-Disposition: inline" gs://www.$BUCKET_NAME/**/*.html
